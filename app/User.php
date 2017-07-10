0162878156ed3553a997d88ad0b42480bd915dcc
Dongkun Lee <clap4ya@gmail.com>
2017-07-10 14:06:28 +0900

    Master _ INIT

---
+++ app/User.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $table = 'customers';
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'confirm_code',
+        'activated',
+        'notification',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+        'confirm_code',
+        'last_login',
+        'activated',
+        'updated_at',
+    ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'last_login',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'activated' => 'boolean',
+    ];
+
+    /* Relationships */
+
+    public function articles() {
+        return $this->hasMany(Article::class,'articles_user_id_foreign');
+    }
+
+    public function comments() {
+        return $this->hasMany(Comment::class,'comments_user_id_foreign');
+    }
+
+    public function votes()
+    {
+        return $this->hasMany(Vote::class,'votes_user_id_foreign');
+    }
+
+    /* Query Scopes */
+
+    public function scopeSocialUser(\Illuminate\Database\Eloquent\Builder $query, $email)
+    {
+        return $query->whereEmail($email)->whereNull('password')->whereActivated(1);
+    }
+
+    /* Accessors */
+
+//    public function getGravatarUrlAttribute()
+//    {
+//        return sprintf("//www.gravatar.com/avatar/%s?s=%s", md5($this->email), 48);
+//    }
+
+    /* Helpers */
+
+    public function isAdmin()
+    {
+        return $this->id === 1;
+    }
+
+    public function isSocialUser()
+    {
+        return is_null($this->password) && $this->activated;
+    }
+}
